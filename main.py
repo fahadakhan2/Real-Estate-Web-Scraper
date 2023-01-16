04d4fabd32098948972cd90e0a6a6fdcb9ca5134
fahadakhan2 <kanlifahad@gmail.com>
2023-01-16 14:59:20 -0600

    Fixed the print statements some more to get the format right Fixed the empty lines issue that was occuring when a specific listing wouldn't have one of the variables so it would print an empty line. Put those variables in if statements to only print them if they were there on the listings for example:
    826 Humboldt Ave, Winnetka, IL 60093
    Status: For Sale
    Price: $855,000
     8,276sqft lot
    
    Missing the beds and the baths but didn't end up printing empty line.

--- main.py
+++ main.py
@@ -28,21 +28,28 @@ for home in homes:
     square_feet = extract_text(home, 'pc-meta-sqft')
     acre_lot = extract_text(home, 'pc-meta-sqftlot')
 
-    print(f'{addresses}')  # Changed the prints again for better alignment still need to work on the spacing for the houses that dont list any of the 4 specific variables
+    print(f'{addresses}')  #  work on the spacing for the houses that dont list any of the 4 specific variables
     print(f'Status: {status_texts}')
     print(f'Price: {prices}')
-    print(f'{beds} {baths}')
-    print(f'{square_feet} {acre_lot}')
-
+    if beds or baths:
+        print(f'{beds} {baths}', end=' ')
+        print()
+    if square_feet or acre_lot:
+        print(f'{square_feet} {acre_lot}')
+    # if beds:   //For having each variable on its own line for storing later in dataframe or base.
+    #     print(f'{beds}')
+    # if baths:
+    #     print(f'{baths}')
+    # if square_feet:
+    #     print(f'{square_feet}')
+    # if acre_lot:
+    #     print(f'{acre_lot}')
     print('')
 
-    # print(f'''
-    # {addresses}
-    # Status: {status_texts}
-    # Price : {prices}
-    # {beds} {baths}
-    # {square_feet} {acre_lot} 
-    # ''')
+
+
+
+
 
 
 
