a437c739b5fe2b5b17e512921a2d76b0d08e3fa9
fahadakhan2 <kanlifahad@gmail.com>
2023-01-16 22:43:17 -0600

    Implemented A Filtration System for Price The user gets to choose a price where he wants to see only houses equal to or more than in price. Changed the input to return an int and also changed the prices variable to return an int.

--- main.py
+++ main.py
@@ -17,31 +17,40 @@ driver = webdriver.Chrome()
 driver.get('https://www.realtor.com/realestateandhomes-search/Winnetka_IL')
 html_text = driver.page_source # Get the HTML source code
 
+# Filtration System
+print('Input the minimum amount you want to buy a house like this -> 1000000 (for one million)')
+price_filter = input('>')
+price_filter = int(price_filter) #makes the input a floating point number
+print(f'Retrieving houses over {price_filter}')
+
 soup = BeautifulSoup(html_text, 'lxml')
 homes = soup.find_all('li', class_ = 'jsx-1881802087 component_property-card') # For all home listings on first page
 for home in homes: 
     addresses = home.find('div', class_ = 'jsx-11645185 address ellipsis srp-page-address srp-address-redesign').text
     status_texts = home.find('span', class_ = 'jsx-3853574337 statusText').text
+    
     prices = home.find('span', attrs={'class': 'Price__Component-rui__x3geed-0 gipzbd', 'data-label': 'pc-price'}).text
+    prices = int(prices.replace('$', '').replace(',', '')) # removes the $ sign and commas and convert to float
+
     beds = extract_text(home, 'pc-meta-beds')
     baths = extract_text(home, 'pc-meta-baths')
     square_feet = extract_text(home, 'pc-meta-sqft')
     acre_lot = extract_text(home, 'pc-meta-sqftlot')
     more_info_link = home.div.a['href'] # to get the href link for more information on each house
-   
+    
+    if prices >= price_filter:
+        print(f'{addresses}') 
+        print(f'Status: {status_texts}')
+        print(f'Price: ${prices}')
+        if beds or baths:
+            print(f'{beds} {baths}', end=' ')
+            print()
+        if square_feet or acre_lot:
+            print(f'{square_feet} {acre_lot}')
+        print(f'More Info: {more_info_link}')
 
-    print(f'{addresses}') 
-    print(f'Status: {status_texts}')
-    print(f'Price: {prices}')
-    if beds or baths:
-        print(f'{beds} {baths}', end=' ')
-        print()
-    if square_feet or acre_lot:
-        print(f'{square_feet} {acre_lot}')
-    print(f'More Info: {more_info_link}')
-
-  
-    print('')
+    
+        print('')
 
 
 
